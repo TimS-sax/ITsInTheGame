052b48fa73240f0a409845182c3b08e00ffdf086
Matseuhh <brunoduistermaat@gmail.com>
2024-11-22 10:53:31 +0100

    Changed dimentions of the SaxionApp

--- BasicGame/src/BasicGame.java
+++ BasicGame/src/BasicGame.java
@@ -10,11 +10,11 @@ import java.util.ArrayList;
 public class BasicGame implements GameLoop {
 
     public static void main(String[] args) {
-        SaxionApp.startGameLoop(new BasicGame(), 1280, 852, 40);
+        SaxionApp.startGameLoop(new BasicGame(), 1280, 800, 40);
     }
 
     public void background() {
-        SaxionApp.drawImage("BasicGame/resources/background.jpg", 0,0, 1280,852);
+        SaxionApp.drawImage("BasicGame/resources/background.jpg", 0,0, 1280,800);
     }
 
     public void startScreen() {
